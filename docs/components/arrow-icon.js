18facb90a326dee283ff9c19298799b246378315
Valery Tretyak <valery@tretyak.net>
2025-02-02 22:21:42 +0100

    About section redesign

---
+++ docs/components/arrow-icon.js
@@ -0,0 +1,47 @@
+class ArrowIcon extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    const size = this.getAttribute("size") || "43";
+    const color = this.getAttribute("color") || "#171E25";
+    const strokeWidth = this.getAttribute("stroke-width") || "2.04484";
+
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: inline-block;
+          width: ${size}px;
+          height: ${size}px;
+        }
+        svg {
+          width: 100%;
+          height: 100%;
+        }
+      </style>
+      
+      <svg 
+        viewBox="0 0 43 43" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="21.5"
+          cy="21.5"
+          r="18.8125"
+          stroke="${color}"
+          stroke-width="${strokeWidth}"
+        />
+        <path
+          d="M20.7241 12.5024H30.4976M30.4976 12.5024V22.2759M30.4976 12.5024L10.8672 32.1328"
+          stroke="${color}"
+          stroke-width="${strokeWidth}"
+        />
+      </svg>
+    `;
+  }
+}
+
+customElements.define("arrow-icon", ArrowIcon);
